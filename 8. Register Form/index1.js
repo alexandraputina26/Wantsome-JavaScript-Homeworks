768beba378075d3d155e594a0988550bac803c0d
Alexandra Putina <pahomiflorinaalexandra@gmail.com>
2019-05-05 12:42:44 +0300

    added homework 8

---
+++ 8. Register Form/index1.js
@@ -0,0 +1,44 @@
+
+
+
+// function alphaNumeric(key) { 
+
+//     for (let i = 0; i < key.length; i++) {
+//         if (key.charCodeAt(i) < 48 && key.charCodeAt(i) > 122) {
+//             alert('Please input alphanumeric characters only'); 
+//         } else {
+//             alert('Your registration is accepted');
+//         }
+//     }
+// }
+
+// function phoneFunction() {
+//     const phone = document.getElementById("Phone").value;
+//     if (phone.length < 10) {
+//         alert("the number should be longer")
+//     } else {
+//         alert("phone number accepted")
+//     }
+// }
+
+
+// frmvalidator.addValidation("FirstName","maxlen=20",	"Max length for FirstName is 20");
+// frmvalidator.addValidation("FirstName","alpha","Alphabetic chars only");
+      
+// frmvalidator.addValidation("LastName","req","Please enter your Last Name");
+// frmvalidator.addValidation("LastName","maxlen=20","Max length is 20");
+            
+            
+// frmvalidator.addValidation("Phone","maxlen=50");
+// frmvalidator.addValidation("Phone","numeric");
+            
+// frmvalidator.addValidation("Address","maxlen=50");
+// frmvalidator.addValidation("Country","dontselect=000");
+
+
+
+    // const frmvalidator = new Validator("myform");
+    // frmvalidator.addValidation("UserName","req","Please enter your user name");
+    // frmvalidator.addValidation("Email","maxlen=50");
+    // frmvalidator.addValidation("Email","req");
+    // frmvalidator.addValidation("Email","email");
