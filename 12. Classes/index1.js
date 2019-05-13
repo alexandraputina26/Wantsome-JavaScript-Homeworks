c89592f8d0d71bf5cd4daa370f603580e91a192f
Alexandra Putina <pahomiflorinaalexandra@gmail.com>
2019-05-13 19:09:19 +0300

    edited in class

--- 12. Classes/index1.js
+++ 12. Classes/index1.js
@@ -138,10 +138,56 @@ let eleph = new Elephant('Mitzie')
 eleph.showBehaviour();
 
 //4. Adaugati 4 metode noi:
-//   walking - $name is walking
-//   rage - $name is rageing 
-//   showBehaviour - returnati metodele eatFood si rage
-//   dailyRoutine - returnati metodele weakingUp, rage, eatFood si sleep
+ // walking - $name is walking
+//  rage - $name is rageing 
+ // showBehaviour - returnati metodele eatFood si rage
+ // dailyRoutine - returnati metodele weakingUp, rage, eatFood si sleep
 
-//5. Creati o constanta noua din clasa cu numele de animal pe care ati scris-o mai sus( NU DIN clasa Animal ci din clasa Elephant, Monkey, Lion etc etc);
-//6. Accesati toate cele 4 metode de pe noul animal. 
+// 5. Creati o constanta noua din clasa cu numele de animal pe care ati scris-o mai sus( NU DIN clasa Animal ci din clasa Elephant, Monkey, Lion etc etc);
+// 6. Accesati toate cele 4 metode de pe noul animal. 
+
+
+
+
+//1. creati o clasa Account care sa aiba location si name la constructor
+//2. creati o clasa Transaction care sa iba sender, bank, amount si reference la constructor
+//2.1 - clasa Transaction sa aiba un getter finalSum() care sa scada din amount 10;
+//3. creati o clasa Table care sa aiba in cunstructor un array gol transactions
+//3.1 Clasa Table sa aiba o metoda addTransaction unde sa putem adauga tranzactiile pe care o sa le creem mai tarziu
+
+class Account {
+  constructor(location, name) {
+    this.location = location;
+    this.name = name;
+  }
+}
+
+class Transaction {
+  constructor(sender, bank, amount, reference) {
+    this.sender = sender;
+    this.bank = bank;
+    this.amount = amount;
+    this.reference = reference;
+  }
+  finalSum() {
+    return this.amount - 10;
+  }
+}
+
+class Table {
+  constructor() {
+    this.transactions = [];
+  }
+  addTransaction(...transaction) {
+    this.transactions.push(...transaction)
+  }
+}
+
+
+
+const bankAccount = new Account('iasi', 'alexandra');
+const transaction1 = new Transaction('Ovidiu', 'ING', 100, 'vacation')
+const transaction2 = new Transaction('Lucian', 'ING', 100, 'school')
+const table = new Table();
+table.addTransaction(transaction1, transaction2);
+console.log(table)
